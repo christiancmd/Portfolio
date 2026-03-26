7d8886d25cc80dac309bd96f8adc620f9c65f294
christian <pariscachristian@gmail.com>
2026-03-26 19:50:53 -0400

    feat: Add metadata in Layout for improving SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://christian-parisca.vercel.app',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
